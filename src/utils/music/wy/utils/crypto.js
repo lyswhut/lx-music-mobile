7f3970b00a9ab499dbc4d89ca62dc3aae0723205
lyswhut <lyswhut@qq.com>
2022-04-02 21:16:46 +0800

    修复wy源搜索某些歌曲时第一页之后的歌曲无法加载的问题

--- src/utils/music/wy/utils/crypto.js
+++ src/utils/music/wy/utils/crypto.js
@@ -1,5 +1,5 @@
 // https://github.com/Binaryify/NeteaseCloudMusicApi/blob/master/util/crypto.js
-import { createCipheriv, publicEncrypt, randomBytes, createHash } from 'crypto'
+import { createCipheriv, createDecipheriv, publicEncrypt, randomBytes, createHash } from 'crypto'
 const iv = Buffer.from('0102030405060708')
 const presetKey = Buffer.from('0CoJUm6Qyw8W8jud')
 const linuxapiKey = Buffer.from('rFgB&h#%2?^eDg:Q')
@@ -8,8 +8,13 @@ const publicKey = '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBi
 const eapiKey = 'e82ckenh8dichen8'
 
 const aesEncrypt = (buffer, mode, key, iv) => {
-  const cipher = createCipheriv('aes-128-' + mode, key, iv)
+  const cipher = createCipheriv(mode, key, iv)
   return Buffer.concat([cipher.update(buffer), cipher.final()])
+}
+
+const aesDecrypt = function(cipherBuffer, mode, key, iv) {
+  let decipher = createDecipheriv(mode, key, iv)
+  return Buffer.concat([decipher.update(cipherBuffer), decipher.final()])
 }
 
 const rsaEncrypt = (buffer, key) => {
@@ -21,7 +26,7 @@ export const weapi = object => {
   const text = JSON.stringify(object)
   const secretKey = randomBytes(16).map(n => (base62.charAt(n % 62).charCodeAt()))
   return {
-    params: aesEncrypt(Buffer.from(aesEncrypt(Buffer.from(text), 'cbc', presetKey, iv).toString('base64')), 'cbc', secretKey, iv).toString('base64'),
+    params: aesEncrypt(Buffer.from(aesEncrypt(Buffer.from(text), 'aes-128-cbc', presetKey, iv).toString('base64')), 'aes-128-cbc', secretKey, iv).toString('base64'),
     encSecKey: rsaEncrypt(secretKey.reverse(), publicKey).toString('hex'),
   }
 }
@@ -33,14 +38,17 @@ export const linuxapi = object => {
   }
 }
 
+
 export const eapi = (url, object) => {
   const text = typeof object === 'object' ? JSON.stringify(object) : object
   const message = `nobody${url}use${text}md5forencrypt`
   const digest = createHash('md5').update(message).digest('hex')
   const data = `${url}-36cd479b6b5-${text}-36cd479b6b5-${digest}`
   return {
-    params: aesEncrypt(Buffer.from(data), 'ecb', eapiKey, '')
-      .toString('hex')
-      .toUpperCase(),
+    params: aesEncrypt(Buffer.from(data), 'aes-128-ecb', eapiKey, '').toString('hex').toUpperCase(),
   }
+}
+
+export const eapiDecrypt = cipherBuffer => {
+  return aesDecrypt(cipherBuffer, 'aes-128-ecb', eapiKey, '').toString()
 }

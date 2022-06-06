d54ea298374f4628bf87fc48582a1d3196631abb
lyswhut <lyswhut@qq.com>
2022-06-06 08:57:29 +0800

    更新依赖

--- src/navigation/components/PactModal.js
+++ src/navigation/components/PactModal.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useState, useEffect } from 'react'
-import { StyleSheet, View, Text, ScrollView } from 'react-native'
+import { StyleSheet, View, Text, ScrollView, Alert } from 'react-native'
 import { Navigation } from 'react-native-navigation'
 
 import Button from '@/components/common/Button'
@@ -21,8 +21,19 @@ const VersionModal = ({ componentId }) => {
 
   const handleConfirm = () => {
     if (!isAgreePact) setAgreePact(true)
-    checkVersion()
     Navigation.dismissOverlay(componentId)
+    setTimeout(() => {
+      Alert.alert(
+        '',
+        Buffer.from('e69cace8bdafe4bbb6e5ae8ce585a8e5858de8b4b9e4b894e5bc80e6ba90efbc8ce5a682e69e9ce4bda0e698afe88ab1e992b1e8b4ade4b9b0e79a84efbc8ce8afb7e79bb4e68ea5e7bb99e5b7aee8af84efbc810a0a5468697320736f667477617265206973206672656520616e64206f70656e20736f757263652e', 'hex').toString(),
+        [{
+          text: Buffer.from('e5a5bde79a8420284f4b29', 'hex').toString(),
+          onPress: () => {
+            checkVersion()
+          },
+        }],
+      )
+    }, 2e3)
   }
 
   const openHomePage = () => {

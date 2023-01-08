39fad04f11ad6060b021b2f8d7ca45636b2121e5
tohsaka888 <156132264@qq.com>
2023-01-08 13:56:06 +0800

    ✨ feat: support new architecture

--- android/app/src/main/jni/MainApplicationModuleProvider.cpp
+++ android/app/src/main/jni/MainApplicationModuleProvider.cpp
@@ -1,9 +1,13 @@
 #include "MainApplicationModuleProvider.h"
+
+#include <rncli.h>
 #include <rncore.h>
+
 namespace facebook {
 namespace react {
+
 std::shared_ptr<TurboModule> MainApplicationModuleProvider(
-    const std::string moduleName,
+    const std::string &moduleName,
     const JavaTurboModule::InitParams &params) {
   // Here you can provide your own module provider for TurboModules coming from
   // either your application or from external libraries. The approach to follow
@@ -14,7 +18,15 @@ std::shared_ptr<TurboModule> MainApplicationModuleProvider(
   //    return module;
   // }
   // return rncore_ModuleProvider(moduleName, params);
+
+  // Module providers autolinked by RN CLI
+  auto rncli_module = rncli_ModuleProvider(moduleName, params);
+  if (rncli_module != nullptr) {
+    return rncli_module;
+  }
+
   return rncore_ModuleProvider(moduleName, params);
 }
+
 } // namespace react
 } // namespace facebook

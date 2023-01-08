e277c30250526555a6676b2c16aac82cca174816
tohsaka888 <156132264@qq.com>
2023-01-08 15:31:08 +0800

    🐛 fix: fix some error

--- android/app/src/main/java/cn/toside/music/mobile/MainActivity.java
+++ android/app/src/main/java/cn/toside/music/mobile/MainActivity.java
@@ -12,10 +12,4 @@ public class MainActivity extends NavigationActivity {
     SplashScreen.show(this, R.style.SplashScreenTheme, true);  // here
     super.onCreate(savedInstanceState);
   }
-  @Override
-    protected boolean isConcurrentRootEnabled() {
-      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
-      // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
-      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-    }
 }

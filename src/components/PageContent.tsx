44c5269c084030c030e9f3f92281658ab38e35db
lyswhut <lyswhut@qq.com>
2023-03-21 17:21:18 +0800

    更新背景高度计算方式

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Dimensions, ImageBackground, type LayoutChangeEvent, View } from 'react-native'
+import { Dimensions, type ScaledSize, View } from 'react-native'
 import { useTheme } from '@/store/theme/hook'
+import ImageBackground from '@/components/common/ImageBackground'
 // import { useDimensions } from '@/utils/hooks'
 
 interface Props {
@@ -11,24 +12,30 @@ interface Props {
 export default ({ children }: Props) => {
   const theme = useTheme()
   // const { window } = useDimensions()
-  const [wh, setWH] = useState<{ width: number | string, height: number | string }>({ width: '100%', height: '100%' })
+  const [wh, setWH] = useState<{ width: number | string, height: number | string }>({ width: '100%', height: Dimensions.get('screen').height })
 
   // 固定宽高度 防止弹窗键盘时大小改变导致背景被缩放
   useEffect(() => {
-    const onChange = () => {
-      setWH({ width: '100%', height: '100%' })
+    const onChange = (event: {
+      window: ScaledSize
+      screen: ScaledSize
+    }) => {
+      setWH({ width: '100%', height: event.screen.height })
     }
 
     const changeEvent = Dimensions.addEventListener('change', onChange)
     return () => { changeEvent.remove() }
   }, [])
-  const handleLayout = (e: LayoutChangeEvent) => {
-    setWH({ width: e.nativeEvent.layout.width, height: e.nativeEvent.layout.height })
-  }
+  // const handleLayout = (e: LayoutChangeEvent) => {
+  //   console.log(e.nativeEvent)
+  //   console.log(Dimensions.get('screen'))
+  //   setWH({ width: e.nativeEvent.layout.width, height: e.nativeEvent.layout.height })
+  // }
+  // console.log('render page content')
 
   return (
     <ImageBackground
-      onLayout={handleLayout}
+      // onLayout={handleLayout}
       style={{ height: wh.height, width: wh.width, backgroundColor: theme['c-content-background'] }}
       source={theme['bg-image']}
       resizeMode="cover"

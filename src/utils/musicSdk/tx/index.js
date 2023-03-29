b481d0367f7e1d0c59fca0d8a27ae247f332c64d
lyswhut <lyswhut@qq.com>
2023-03-29 19:27:32 +0800

    就放tx源歌词获取失败的问题

--- src/utils/musicSdk/tx/index.js
+++ src/utils/musicSdk/tx/index.js
@@ -18,7 +18,7 @@ const tx = {
   },
   getLyric(songInfo) {
     // let singer = songInfo.singer.indexOf('、') > -1 ? songInfo.singer.split('、')[0] : songInfo.singer
-    return lyric.getLyric(songInfo)
+    return lyric.getLyric(songInfo.songmid)
   },
   getPic(songInfo) {
     return apis('tx').getPic(songInfo)

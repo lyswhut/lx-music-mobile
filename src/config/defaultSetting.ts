b644a938f3211192dcd5379e4084ad1cc604bf02
lyswhut <lyswhut@qq.com>
2023-05-13 12:51:23 +0800

    更新默认主题设置

--- src/config/defaultSetting.ts
+++ src/config/defaultSetting.ts
@@ -60,7 +60,8 @@ const defaultSetting: LX.AppSetting = {
 
   'sync.enable': false,
 
-  'theme.id': 'blue_plus',
+  // 'theme.id': 'blue_plus',
+  'theme.id': 'green',
   'theme.lightId': 'green',
   'theme.darkId': 'black',
   'theme.hideBgDark': false,

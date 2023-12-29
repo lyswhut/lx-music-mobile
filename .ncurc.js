2996d6bdf24db8c07b2d8c314d856862244945bd
lyswhut <lyswhut@qq.com>
2023-12-29 14:58:04 +0800

    添加 Android 5 特别版与墨·状态栏特别版的 release 构建

--- .ncurc.js
+++ .ncurc.js
@@ -1,10 +1,7 @@
 module.exports = {
   upgrade: true,
   reject: [
-    // 'metro-react-native-babel-preset',
     '@types/react-native',
-    'metro-react-native-babel-preset',
-    '@react-native/metro-config',
     'react',
   ],
 
@@ -12,7 +9,6 @@ module.exports = {
   // filter: [
   //   '@types/react-native',
   //   'react',
-  //   'metro-react-native-babel-preset',
   //   '@react-native/metro-config',
   // ],
 }

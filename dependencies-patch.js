a1c0a62d3fd5091d94a2df4579ab1da34fcf2008
lyswhut <lyswhut@qq.com>
2024-02-20 21:49:35 +0800

    更新依赖

--- dependencies-patch.js
+++ dependencies-patch.js
@@ -6,51 +6,6 @@ const path = require('node:path')
 const rootPath = path.join(__dirname, './')
 
 const patchs = [
-  [
-    path.join(rootPath, './node_modules/react-native-navigation/lib/android/app/build.gradle'),
-    `
-    compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8
-    }`,
-    '',
-  ],
-  [
-    path.join(rootPath, './node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/stack/topbar/button/ButtonPresenter.kt'),
-    `override fun onInitializeAccessibilityNodeInfo(
-                        host: View?,
-                        info: AccessibilityNodeInfoCompat?
-                    ) {
-                        super.onInitializeAccessibilityNodeInfo(host, info)
-
-                        // Expose the testID prop as the resource-id name of the view. Black-box E2E/UI testing
-                        // frameworks, which interact with the UI through the accessibility framework, do not have
-                        // access to view tags. This allows developers/testers to avoid polluting the
-                        // content-description with test identifiers.
-                        val testId = host?.tag as String?
-                        if(testId != null){
-                            info!!.viewIdResourceName = testId
-                        }
-                    }`,
-    `override fun onInitializeAccessibilityNodeInfo(
-                      host: View,
-                      info: AccessibilityNodeInfoCompat
-                    ) {
-                        super.onInitializeAccessibilityNodeInfo(host, info)
-
-                        // Expose the testID prop as the resource-id name of the view. Black-box E2E/UI testing
-                        // frameworks, which interact with the UI through the accessibility framework, do not have
-                        // access to view tags. This allows developers/testers to avoid polluting the
-                        // content-description with test identifiers.
-                        val testId = host.tag as String?
-                        if(testId != null){
-                            info.viewIdResourceName = testId
-                        }
-                    }`,
-  ],
 ]
 
 ;(async() => {

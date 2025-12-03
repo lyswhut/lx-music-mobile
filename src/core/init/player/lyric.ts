8b896b686594e9a588e45f7013e899a1308e5fbf
qwscqq <qwscqq@163.com>
2025-12-03 17:04:19 +0800

    Update lyric.ts
    蓝牙歌词多行显示

--- src/core/init/player/lyric.ts
+++ src/core/init/player/lyric.ts
@@ -6,52 +6,62 @@ import { updateNowPlayingTitles } from '@/plugins/player/utils'
 import { setLastLyric } from '@/core/player/playInfo'
 import { state } from '@/plugins/player/playList'
 
-const updateRemoteLyric = async(lrc?: string) => {
-  setLastLyric(lrc)
-  if (lrc == null) {
-    void updateNowPlayingTitles((state.prevDuration || 0) * 1000, playerState.musicInfo.name, playerState.musicInfo.singer ?? '', playerState.musicInfo.album ?? '')
+const updateRemoteLyric = async(lrcLines?: string[]) => {
+  setLastLyric(lrcLines ? lrcLines.join('\n') : null);
+  if (lrcLines == null || lrcLines.length === 0) {
+    void updateNowPlayingTitles(
+      (state.prevDuration || 0) * 1000,
+      playerState.musicInfo.name,
+      playerState.musicInfo.singer ?? '',
+      playerState.musicInfo.album ?? ''
+    );
   } else {
-    void updateNowPlayingTitles((state.prevDuration || 0) * 1000, lrc, `${playerState.musicInfo.name}${playerState.musicInfo.singer ? ` - ${playerState.musicInfo.singer}` : ''}`, playerState.musicInfo.album ?? '')
+    const formattedLyrics = lrcLines.join('\n');
+    void updateNowPlayingTitles(
+      (state.prevDuration || 0) * 1000,
+      formattedLyrics,
+      `${playerState.musicInfo.name}${playerState.musicInfo.singer ? ` - ${playerState.musicInfo.singer}` : ''}`,
+      playerState.musicInfo.album ?? ''
+    );
   }
-}
+};
 
 export default async(setting: LX.AppSetting) => {
-  await initLyricPlayer()
+  await initLyricPlayer();
   await Promise.all([
     setPlaybackRate(setting['player.playbackRate']),
     toggleTranslation(setting['player.isShowLyricTranslation']),
     toggleRoma(setting['player.isShowLyricRoma']),
-  ])
+  ]);
 
   if (setting['desktopLyric.enable']) {
     showDesktopLyric().catch(() => {
-      updateSetting({ 'desktopLyric.enable': false })
-    })
+      updateSetting({ 'desktopLyric.enable': false });
+    });
   }
   if (setting['player.isShowBluetoothLyric']) {
     showRemoteLyric(true).catch(() => {
-      updateSetting({ 'player.isShowBluetoothLyric': false })
-    })
+      updateSetting({ 'player.isShowBluetoothLyric': false });
+    });
   }
   onDesktopLyricPositionChange(position => {
     updateSetting({
       'desktopLyric.position.x': position.x,
       'desktopLyric.position.y': position.y,
-    })
-  })
+    });
+  });
   onLyricLinePlay(({ text, extendedLyrics }) => {
     if (!text && !state.isPlaying) {
-      void updateRemoteLyric()
+      void updateRemoteLyric();
     } else {
-      void updateRemoteLyric(text)
+      void updateRemoteLyric(extendedLyrics || [text]);
     }
-  })
+  });
 
-
-  global.app_event.on('play', play)
-  global.app_event.on('pause', pause)
-  global.app_event.on('stop', stop)
-  global.app_event.on('error', pause)
-  global.app_event.on('musicToggled', stop)
-  global.app_event.on('lyricUpdated', setLyric)
-}
+  global.app_event.on('play', play);
+  global.app_event.on('pause', pause);
+  global.app_event.on('stop', stop);
+  global.app_event.on('error', pause);
+  global.app_event.on('musicToggled', stop);
+  global.app_event.on('lyricUpdated', setLyric);
+};

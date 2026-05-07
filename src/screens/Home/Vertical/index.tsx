106b6fdfecf132f8a34d4ccd593f1fbfe69bf2c6
WIN-KN3VVTK6MKA\EDY <211977992@qq.com>
2026-05-07 17:01:34 +0800

    feat(ui): 新增底部 BottomTabBar 与 PlayerBar 卡片样式
    Co-authored-by: Pride-lee <211977992@qq.com>

--- src/screens/Home/Vertical/index.tsx
+++ src/screens/Home/Vertical/index.tsx
@@ -1,11 +1,13 @@
 import Content from './Content'
 import PlayerBar from '@/components/player/PlayerBar'
+import BottomTabBar from '../components/BottomTabBar'
 
 export default () => {
   return (
     <>
       <Content />
       <PlayerBar isHome />
+      <BottomTabBar />
     </>
   )
 }
